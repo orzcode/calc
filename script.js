cad5c66283f962685f0a1ba6e45425f41ee7817a
orzcode <71000311+orzcode@users.noreply.github.com>
2023-01-18 17:02:45 +1300

    add basic math JS functions

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-function add(memNum, inputNum){
-	
+let displayValue;
+let memoryValue;
+//////////
+let display = document.querySelector('#display');
+let memory = document.querySelector('#memory');
+
+//////////
+
+//////////
+function add(value1, value2){
+	return value1 + value2;
 }
+
+function subtract(value1, value2){
+	return value1 - value2;
+}
+
+function multiply(value1, value2){
+	return value1 * value2;
+}
+
+function divide(value1, value2){
+	if (value2 === 0){
+		return "Reality Breaks";
+	}else
+	return value1 / value2;
+}
+/////////
+function operate(operator, value1, value2){
+	return operator(value1, value2);
+}
+/////////
+function keyedNumber(keyedNumber){
+	display.insertAdjacentText('beforeend', keyedNumber);
+}
+/////////
+function clr(){
+	display.innerHTML = "";
+	memory.innerHTML = "";
+}
+/////////
