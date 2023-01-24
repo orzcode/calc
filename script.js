f9f9826f24be8b1dcefa62992913d9163ee5bd4f
orzcode <71000311+orzcode@users.noreply.github.com>
2023-01-24 18:06:30 +1300

    make darkmode functional (styling TBC)

--- script.js
+++ script.js
@@ -131,3 +131,17 @@ window.addEventListener("keydown", (e) => {
         case "Enter": equals(operatorType); break;
     }
 })
+///////////
+//DARKMODE TOGGLE//
+const toggleSwitch = document.querySelector('.darkmode-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
