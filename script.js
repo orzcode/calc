3c49d203e304f1004da8ed89761bfe50273a5870
orzcode <71000311+orzcode@users.noreply.github.com>
2023-01-31 15:16:34 +1300

    'double operator' quirk fully fixed

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ let resultDisplayed = false;
 let newOperatorType;
 let divideByZero = false;
 let doubleOperator = false;
+let tempMemVal;
 //////////
-function opManager(type){
-	switch(type){
+function opManager(type) {
+	switch (type) {
 		case "add":
 			add(memoryValue, displayValue);
 			break;
@@ -37,108 +38,107 @@ function opManager(type){
 
 ////////////////////////////
 //the raw equation functions//
-function add(value1, value2){
+function add(value1, value2) {
 	result = Number(value1) + Number(value2);
 	return result;
 }
-function subtract(value1, value2){
+function subtract(value1, value2) {
 	result = Number(value1) - Number(value2);
 	return result;
 }
-function multiply(value1, value2){
+function multiply(value1, value2) {
 	result = Number(value1) * Number(value2);
 	return result;
 }
-function divide(value1, value2){
-	if (Number(value2) === 0){
+function divide(value1, value2) {
+	if (Number(value2) === 0) {
 		divideByZero = true;
 		return divideByZero;
-	}else
-	result = Number(value1) / Number(value2);
+	} else
+		result = Number(value1) / Number(value2);
 	return result;
 }
 ///////////////////////////
-function operation(operator, symbol){
+function operation(operator, symbol) {
 	hasDecimal = false;
-	
-	if(doubleOperator === true){
+
+	if (doubleOperator === true) {
 		memory.innerHTML = memoryValue + " " + symbol;
+		tempMemVal = memoryValue;
 		operatorType = operator;
-	}else{
+	} else {
 
-	if(opInProgress === true){
-		opManager(operatorType);
-		console.log("'Result' after doing a 'switched' (eg 2nd) operation: " + result);
-		memoryValue = result;
-		memory.innerHTML = parseFloat(result.toFixed(4)) + " " + symbol;
-		display.innerHTML = 0; //do I really want to clear display? OR retain original value?
-		operatorType = operator;
-		doubleOperator = true;
-	}else 
+		if (opInProgress === true) {
+			opManager(operatorType);
+			console.log("'Result' after doing a 'switched' (eg 2nd) operation: " + result);
+			memoryValue = result;
+			memory.innerHTML = parseFloat(result.toFixed(4)) + " " + symbol;
+			display.innerHTML = 0; //do I really want to clear display? OR retain original value?
+			operatorType = operator;
+			doubleOperator = true;
+		} else
 
-	if(opInProgress === false){
-		opInProgress = true;
-		memory.innerHTML = displayValue + " " + symbol;
-		memoryValue = displayValue;
-		operatorType = operator;
-		display.innerHTML = 0; //do I really want to clear display? OR retain original value?
+			if (opInProgress === false) {
+				opInProgress = true;
+				memory.innerHTML = displayValue + " " + symbol;
+				memoryValue = displayValue;
+				operatorType = operator;
+				display.innerHTML = 0; //do I really want to clear display? OR retain original value?
+			}
+		//Displays message IF divided by zero//
+		if (divideByZero === true) {
+			display.innerHTML = "Reality Broken";
+			divideByZero = false;
+		}
 	}
-	//Displays message IF divided by zero//
-	if(divideByZero === true){
-		display.innerHTML = "Reality Broken";
-		divideByZero = false;
-	}
-}
 	//Displays message IF divided by zero//
 	return operatorType;
 }
 /////////
-function equals(type){
-	if(doubleOperator === true){
-		displayValue = memoryValue;
-		display.innerHTML = memoryValue;
-		clr("equalsType"); //THIS NEEDS FIXING
-	}else{
-	opManager(type);
-	clr("equalsType");
-	displayValue = parseFloat(result.toFixed(4));
-	display.innerHTML = parseFloat(result.toFixed(4));
-	//rounds to 4 decimal places, and lops off extra zeroes
+function equals(type) {
+	if (doubleOperator === true) {
+		clr("equalsType");
+		displayValue = tempMemVal;
+		display.innerHTML = tempMemVal;
+		resultDisplayed = true;
+		//only activates when operator btn pressed twice in sucession
+	} else {
+		opManager(type);
+		clr("equalsType");
+		displayValue = parseFloat(result.toFixed(4));
+		display.innerHTML = parseFloat(result.toFixed(4));
+		//rounds to 4 decimal places, and lops off extra zeroes
 
-	//Displays message IF divided by zero//
-	if(divideByZero == true){
-		display.innerHTML = "Reality Broken";
-		divideByZero = false;
+		//Displays message IF divided by zero//
+		if (divideByZero == true) {
+			display.innerHTML = "Reality Broken";
+			divideByZero = false;
+		}
+
+		//Displays message IF divided by zero - flag resets itself, so not needed during clr//
+
+		result = displayValue;
+		resultDisplayed = true;
+		opInProgress = false;
+		console.log("After hitting Equals, result var is: " + result + " and type is " + typeof result);
+		console.log("After hitting Equals, opInProgress var is: " + opInProgress);
 	}
-
-	//Displays message IF divided by zero - flag resets itself, so not needed during clr//
-
-	result = displayValue;
-	resultDisplayed = true;
-	opInProgress = false;
-	console.log("After hitting Equals, result var is: " + result + " and type is " + typeof result);
-	console.log("After hitting Equals, opInProgress var is: " + opInProgress);
-}
 }
 /////////
-function keyedNumber(keyedNumber){
-	// if (opInProgress === false){
-	// 	displayValue = '';
-	// 	opInProgress = true;
-	// }
-	if(display.innerHTML === "Reality Broken"){
+function keyedNumber(keyedNumber) {
+	if (display.innerHTML === "Reality Broken") {
 		display.innerHTML = '';
 		//allows over-writing with digits when division message is shown
 	}
 
-	if (display.innerHTML === "0"){
+	if (display.innerHTML === "0") {
 		display.innerHTML = '';
 	}
-	if (display.innerHTML === "0."){
+	if (display.innerHTML === "0.") {
 		display.innerHTML = '0.';
 	}
 	////////
-	if (resultDisplayed === true){
+	if (resultDisplayed === true) {
 		display.innerHTML = '';
 		resultDisplayed = false;
 	}
@@ -158,8 +158,8 @@ function keyedNumber(keyedNumber){
 // Appends the keyed number to the right of any current numbers
 // in the Display, and then sets the full Display value var
 /////////
-function decimal(){
-	switch(hasDecimal){
+function decimal() {
+	switch (hasDecimal) {
 		case false:
 			hasDecimal = true;
 			display.insertAdjacentText('beforeend', '.');
@@ -172,8 +172,8 @@ function decimal(){
 	}
 }
 /////////
-function clr(type){
-	if (type !== "equalsType"){
+function clr(type) {
+	if (type !== "equalsType") {
 		result = 0;
 		hasDecimal = false;
 		//checks to see if the answer (after an Equals operation) has a decimal - if so, prevents you from adding another
@@ -181,7 +181,7 @@ function clr(type){
 	display.innerHTML = "0";
 	memory.innerHTML = '';
 	displayValue = 0;
-	memoryValue = 0;	
+	memoryValue = 0;
 	operatorType = '';
 	opInProgress = false;
 	doubleOperator = false;
@@ -192,24 +192,24 @@ function clr(type){
 /*--Keyboard Numpad functionality--*/
 /////////
 window.addEventListener("keydown", (e) => {
-    switch (e.key) {
-        case "9": keyedNumber(9); break;
-        case "8": keyedNumber(8); break;
-        case "7": keyedNumber(7); break;
-        case "6": keyedNumber(6); break;
-        case "5": keyedNumber(5); break;
-        case "4": keyedNumber(4); break;
-        case "3": keyedNumber(3); break;
-        case "2": keyedNumber(2); break;
-        case "1": keyedNumber(1); break;
-        case "0": keyedNumber(0); break;
-        case "+": operation('add', '+'); break;
-        case "-": operation('subtract', '-'); break;
-        case "*": operation('multiply', '×'); break;
-        case "/": operation('divide', '÷'); break;
-        case ".": decimal(); break;
-        case "Enter": equals(operatorType); break;
-    }
+	switch (e.key) {
+		case "9": keyedNumber(9); break;
+		case "8": keyedNumber(8); break;
+		case "7": keyedNumber(7); break;
+		case "6": keyedNumber(6); break;
+		case "5": keyedNumber(5); break;
+		case "4": keyedNumber(4); break;
+		case "3": keyedNumber(3); break;
+		case "2": keyedNumber(2); break;
+		case "1": keyedNumber(1); break;
+		case "0": keyedNumber(0); break;
+		case "+": operation('add', '+'); break;
+		case "-": operation('subtract', '-'); break;
+		case "*": operation('multiply', '×'); break;
+		case "/": operation('divide', '÷'); break;
+		case ".": decimal(); break;
+		case "Enter": equals(operatorType); break;
+	}
 })
 //////////////////////////////////////
 
@@ -217,12 +217,12 @@ window.addEventListener("keydown", (e) => {
 const toggleSwitch = document.querySelector('.darkmode-switch input[type="checkbox"]');
 
 function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }    
+	if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+	}
+	else {
+		document.documentElement.setAttribute('data-theme', 'light');
+	}
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
